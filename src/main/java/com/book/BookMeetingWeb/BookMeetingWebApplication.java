e393477bc2f5fc56624fd4c31270f6af7336dcd0
MinJeong <106390284+Mineong@users.noreply.github.com>
2025-05-27 11:17:14 +0900

    initial

---
+++ src/main/java/com/book/BookMeetingWeb/BookMeetingWebApplication.java
@@ -0,0 +1,13 @@
+package com.book.BookMeetingWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMeetingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMeetingWebApplication.class, args);
+
+	}
+}
---
+++ src/test/java/com/book/BookMeetingWeb/BookMeetingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.BookMeetingWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMeetingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
