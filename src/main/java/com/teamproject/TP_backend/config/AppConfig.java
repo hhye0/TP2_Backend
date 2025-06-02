1b3bae0756e76ddc4bc6dffcb96130f430e27815
hhye0 <jhy030123@naver.com>
2025-06-02 19:54:53 +0900

    Lazyãy error soultion

--- src/main/java/com/teamproject/TP_backend/config/AppConfig.java
+++ src/main/java/com/teamproject/TP_backend/config/AppConfig.java
@@ -2,9 +2,11 @@ package com.teamproject.TP_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableTransactionManagement
 public class AppConfig {
 
     @Bean
