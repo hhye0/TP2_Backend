9278c17762257653f9959f66a3535cead8957898
hhye0 <jhy030123@naver.com>
2025-06-04 13:54:41 +0900

    front correction

--- src/main/java/com/teamproject/TP_backend/controller/dto/BookDTO.java
+++ src/main/java/com/teamproject/TP_backend/controller/dto/BookDTO.java
@@ -1,21 +1,19 @@
 package com.teamproject.TP_backend.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookDTO {
-    @JsonProperty("isbn13")
+
+    @JsonProperty("isbn13")  // JSON 필드명이 isbn13일 경우 매핑
     private String isbn;
+
     private String title;
     private String author;
     private String cover;
-
-    // 생성자
-    public BookDTO(String isbn, String title, String author, String cover) {
-        this.isbn = isbn;
-        this.title = title;
-        this.author = author;
-        this.cover = cover;
-    }
 }
